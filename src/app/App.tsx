7d0c3a506d3ca179a49738c796d778cae26960e2
Amr El Mahdy <amrelmhdy@hotmail.com>
2025-03-19 01:59:18 +0200

    basic folder structure

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigation from '../navigation';
+
+function App(): React.JSX.Element {
+    return (
+        <Navigation />
+    );
+}
+
+
+
+export default App;
